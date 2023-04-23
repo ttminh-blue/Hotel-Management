3ace107d632e9f0e76e86ba8f2a29bca069a1147
minhnhox792@gmail <minhnhox792@gmail.com>
2023-04-23 14:34:26 +0700

    API Doan, Yeucaudacbiet

---
+++ lazy-days-API/Controllers/DoanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lazy_days_API.Controllers
+{
+    public class DoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
