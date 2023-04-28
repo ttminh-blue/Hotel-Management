aa1454b59fa580d15638cef414140d4596da02e9
minhnhox792@gmail <minhnhox792@gmail.com>
2023-04-28 17:01:27 +0700

    Push to invoice

--- lazy-days/pages/service/receipt.tsx
+++ lazy-days/pages/service/receipt.tsx
@@ -1,30 +1,19 @@
 import DefaultLayout from "@/layouts/DefaultLayout";
 import { Card } from "flowbite-react";
 import React, { useState, useEffect } from "react";
+import { useRouter } from 'next/router'
 
-const invoiceData = {
-   customerDetails: {
-      name: "John Doe",
-      address: "123 Main St, New York, NY 10001",
-      phone: "+1 (123) 456-7890",
-   },
-   issueDate: "2023-03-23",
-   paymentMethod: "Credit Card",
-   servicesUsed: [
-      { name: "Room Service", price: 30 },
-      { name: "Laundry", price: 20 },
-   ],
-   roomDetails: {
-      roomNumber: "101",
-      roomType: "Deluxe",
-      checkInDate: "2023-03-20",
-      checkOutDate: "2023-03-23",
-   },
-   totalAmount: 250,
-};
 type Props = {};
 
 const Receipt = () => {
+
+
+   const [invoiceData , setInvoiceData] = useState<any>([]);
+   useEffect(() => {
+      const invoice = JSON.parse(sessionStorage.receipt);
+      console.log("My invoice: ", invoice)
+      setInvoiceData(invoice)
+   }, [])
    return (
       <DefaultLayout>
          <div>
@@ -35,9 +24,9 @@ const Receipt = () => {
                   <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
                      <div className="flex justify-between">
                         <h1 className="text-3xl font-bold text-blue-600">
-                           Receipt
+                           Invoice
                         </h1>
-                        <img src="https://via.placeholder.com/60" alt="Logo" />
+                        
                      </div>
 
                      <div className="mb-10">
@@ -47,30 +36,31 @@ const Receipt = () => {
                         <div className="ml-5">
                            <p className="mb-1">
                               <span className="font-semibold">Name:</span>{" "}
-                              {invoiceData.customerDetails.name}
+                              {invoiceData.TEN_KH}
                            </p>
                            <p className="mb-1">
                               <span className="font-semibold">
                                  Phone Number:
-                              </span>
-                              {invoiceData.customerDetails.phone}
+                              </span>{" "}
+                              {invoiceData.SDT}
                            </p>
                            <p className="mb-1">
-                              <span className="font-semibold">Address:</span>
-                              {invoiceData.customerDetails.address}
+                              <span className="font-semibold">Address:</span>{" "}
+                              {invoiceData.DIA_CHI}
+                           </p>
+                           <p className="mb-1">
+                              <span className="font-semibold">Type room:</span>{" "}
+                              {invoiceData.LOAIPHONG}
                            </p>
                         </div>
                      </div>
 
                      <div className="mb-4">
-                        <p className="font-semibold">Issue Date:</p>
-                        <p>20/03/2023</p>
+                        <p className="font-semibold">Book Date:</p>
+                         {invoiceData.NGAY_DAT}
                      </div>
 
-                     <div className="mb-4">
-                        <p className="font-semibold">Payment Method:</p>
-                        <p>Banking</p>
-                     </div>
+
 
                      <div className="mb-4">
                         <p className="font-semibold">Services Used:</p>
@@ -104,23 +94,15 @@ const Receipt = () => {
                         </table>
                      </div>
 
-                     <div className="mb-4">
-                        <p className="font-semibold">Room Details:</p>
-                        <p>Room Number:P201</p>
-                        <p>
-                           Room Type: <strong>VIP</strong>{" "}
-                        </p>
-                        <p>
-                           Check-In: <strong>3000$</strong>{" "}
-                        </p>
-                        <p>
-                           Check-Out: <strong>7000$</strong>{" "}
-                        </p>
-                     </div>
+                    
 
                      <div className="mb-4 flex justify-end">
                         <p className="font-semibold">Total Amount:</p>
-                        <strong className="ml-10">10000$</strong>
+                        <strong className="ml-10"> {invoiceData.TONG_TIEN}</strong>
+                     </div>
+                     <div className="mb-4 flex justify-end">
+                        <p className="font-semibold">Prepay:</p>
+                        <strong className="ml-10">{invoiceData.TIEN_COC}</strong>
                      </div>
                   </div>
                </div>
--- lazy-days/pages/user/listBookRoom.tsx
+++ lazy-days/pages/user/listBookRoom.tsx
@@ -3,6 +3,8 @@ import { useRef, useState, useEffect, useCallback, useMemo } from "react";
 import axios from "axios";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
+import { useRouter } from 'next/router'
+
 type Props = {};
 // const data_fake = [
 //    {
@@ -54,6 +56,7 @@ type Props = {};
 const Info = (props: Props) => {
    const [data, setData] = useState<any>([]);
    const [phieudatphong, setPhieuDatPhong] = useState<any>([]);
+   const router = useRouter();
    const authFetch = axios.create({
       baseURL: 'https://localhost:44335/api',
    });
@@ -98,6 +101,7 @@ const Info = (props: Props) => {
                new_data[i]['TIEN_COC'] = formatter.format(get_PDD.data[j].TIEN_COC)
                new_data[i]['TONG_TIEN'] = formatter.format(get_PDD.data[j].TONG_TIEN)
                new_data[i]['NGAY_DAT'] = new Date(get_PDD.data[j].NGAY_DAT).toLocaleString()
+               new_data[i]['MA_PHIEU_DP'] = get_PDD.data[j].MA_PHIEU_DP
             }
          }
       }
@@ -130,9 +134,17 @@ const Info = (props: Props) => {
 
    }
    useEffect(() => {
-      console.log("data: ", data)
+
+
       if (makh != null) {
+
+         const receipt_data = data.filter((kh: any) => kh.MA_KH == makh)
+
+         sessionStorage.setItem("receipt", JSON.stringify(receipt_data[0]));
          update_state();
+         router.push(
+            '/service/receipt'
+         )
          console.log("Updated state !!!");
       }
 
@@ -154,8 +166,9 @@ const Info = (props: Props) => {
             <div className="container mx-auto p-4">
                <h1 className="text-2xl font-bold mb-10">Waiting List</h1>
                <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-                  {data.map((item: any, index: any) => (
-                     <div
+                  {data.map((item: any, index: any) => {
+                     if(item.LOAIPHONG == "GUARANTEE" || item.LOAIPHONG == "NOT GUARANTEE"){ 
+                        return <div
                         className="bg-white rounded-lg shadow-md p-4"
                         key={index}
                      >
@@ -196,8 +209,53 @@ const Info = (props: Props) => {
                         >
                            Payment Successfully
                         </button>
+
                      </div>
-                  ))}
+                     }
+                     // <div
+                     //    className="bg-white rounded-lg shadow-md p-4"
+                     //    key={index}
+                     // >
+                     //    <p className="text-lg font-bold mb-2 mt-5">
+                     //       {item.TEN_KH}
+                     //    </p>
+                     //    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                     //       <div>
+                     //          <p className="text-sm text-gray-500 mb-2">
+                     //             <strong>Phone Number:</strong>{" "}
+                     //             {item.SDT}
+                     //          </p>
+                     //          <p className="text-sm text-gray-500 mb-2">
+                     //             <strong> Type room:</strong> {item.LOAIPHONG}
+                     //          </p>
+                     //          <p className="text-sm text-gray-500 mb-2">
+                     //             <strong> Total:</strong> {item.TONG_TIEN}
+                     //          </p>
+                     //       </div>
+                     //       <div>
+                     //          <p className="text-sm text-gray-500 mb-2">
+                     //             <strong>CMND:</strong> {item.CMND}
+                     //          </p>
+                     //          <p className="text-sm text-gray-500 mb-2">
+                     //             <strong>Date:</strong> {item.NGAY_DAT}
+                     //          </p>
+                     //          <p className="text-sm text-gray-500 mb-2">
+                     //             <strong>Prepay:</strong> {item.TIEN_COC}
+                     //          </p>
+                     //       </div>
+                     //    </div>
+                     //    {/* <input type="input" name="makh" value= {item.makh} ref={makhRef} /> */}
+
+                     //    <button className="container bg-blue-500 text-white px-4 py-2 rounded-lg mt-12 btn-custom "
+                     //       onClick={() => { handleClick(item.MA_KH) }}
+
+                     //       type="submit"
+                     //    >
+                     //       Payment Successfully
+                     //    </button>
+
+                     // </div>
+})}
                </div>
             </div>
          </div>
